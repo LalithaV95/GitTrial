65b7be548aa3eddbd55bdcd44fb2bad1b95fba72
l.a.venkatachalam <l.a.venkatachalam@CDC2-D-504Q022.dir.svc.accenture.com>
2018-10-24 15:42:49 +0530

    Added a new class

---
+++ example/src/main/java/com/github/maven/example/GitAddClass.java
@@ -0,0 +1,9 @@
+package com.github.maven.example;
+
+public class GitAddClass {
+	
+	public static void main(String[] args) {
+		System.out.println("Just added a new class to test Git source control");
+	}
+
+}
